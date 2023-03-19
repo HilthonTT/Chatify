c2d9055d06e92eaa936ca3293b7314ad84bb5c55
Timothy Tandt <Timothy.tandt@outlook.be>
2023-03-19 13:24:23 +0100

    Added Sample Data page
    With this page, I created the sample data for the MongoDB for testing purposes.

--- ChatifyLibrary/Models/NotificationModel.cs
+++ ChatifyLibrary/Models/NotificationModel.cs
@@ -7,6 +7,6 @@ public class NotificationModel
     public string Id { get; set; }
     public BasicUserModel Recipient { get; set; }
     public MessageModel Message { get; set; }
-    public DateTime Timestamp { get; set; }
+    public DateTime Timestamp { get; set; } = DateTime.UtcNow;
     public bool IsSeen { get; set; }
 }
