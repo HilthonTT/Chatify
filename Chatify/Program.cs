5bb204ac6352db636982b1f9324532ca93f1586a
Timothy Tandt <Timothy.tandt@outlook.be>
2023-03-19 12:44:29 +0100

    Registered the Services in Program.cs
    Registered the Services using a class called RegisterServices.cs
    
    And deleted default pages.

--- Chatify/Program.cs
+++ Chatify/Program.cs
@@ -1,13 +1,10 @@
-using Chatify.Data;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+using Chatify;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.ConfigureServices();
 
 var app = builder.Build();
 
---
+++ Chatify/RegisterServices.cs
@@ -0,0 +1,27 @@
+﻿using ChatifyLibrary.DataAccess;
+using Microsoft.AspNetCore.ResponseCompression;
+
+namespace Chatify;
+
+public static class RegisterServices
+{
+    public static void ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddRazorPages();
+        builder.Services.AddServerSideBlazor();
+        builder.Services.AddMemoryCache();
+        builder.Services.AddControllersWithViews();
+
+        builder.Services.AddResponseCompression(opts =>
+        {
+            opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+                new[] { "application/octet-stream" });
+        });
+
+        builder.Services.AddSingleton<IDbConnection, DbConnection>();
+        builder.Services.AddSingleton<IConversationData, MongoConversationData>();
+        builder.Services.AddSingleton<IMessageData, MongoMessageData>();
+        builder.Services.AddSingleton<INotificationData, MongoNotificationData>();
+        builder.Services.AddSingleton<IUserData, MongoUserData>();
+    }
+}
