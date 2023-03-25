558667aeba1e7aaea4d63f26bf1451c0fde1b9d7
Timothy Tandt <Timothy.tandt@outlook.be>
2023-03-25 16:18:42 +0100

    Added Create.razor
    Added the code for Create.razor but no HTML has been implemented

---
+++ Chatify/Models/CreateConversationModel.cs
@@ -0,0 +1,20 @@
+﻿using ChatifyLibrary.BasicModel;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Chatify.Models;
+
+public class CreateConversationModel
+{
+    [Required]
+    [MaxLength(100)]
+    public string GroupName { get; set; }
+
+    [Required]
+    [MinLength(1)]
+    [Display(Name = "Category")]
+    public string CategoryId { get; set; }
+
+    [Required]
+    public List<BasicUserModel> Participants { get; set; } = new();
+}
