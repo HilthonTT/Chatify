34f37ec032c0a6ebf9cfc9d666f2b0edb9f75692
Timothy Tandt <Timothy.tandt@outlook.be>
2023-04-05 16:02:29 +0200

    Added a way to see image and text files within the conversation.
    You can now view the image/text files you've sent  to the conversation. If it's a text file, you can download it.

--- ChatifyLibrary/Models/MessageModel.cs
+++ ChatifyLibrary/Models/MessageModel.cs
@@ -9,6 +9,8 @@ public class MessageModel
     public List<BasicUserModel> Receivers { get; set; } = new();
     public string Text { get; set; }
     public string FileName { get; set; }
+    public string OriginalFileName { get; set; }
+    public string FileExtension { get; set; }
     public DateTime Timestamp { get; set; } = DateTime.UtcNow;
     public DateTime LastEdited { get; set; } = DateTime.UtcNow;
     public ConversationModel Conversation { get; set; }
--- ChatifyLibrary/Models/PrivateMessageModel.cs
+++ ChatifyLibrary/Models/PrivateMessageModel.cs
@@ -7,6 +7,9 @@ public class PrivateMessageModel
     public string Id { get; set; }
     public BasicUserModel Sender { get; set; }
     public string Text { get; set; }
+    public string FileName { get; set; }
+    public string OriginalFileName { get; set; }
+    public string FileExtension { get; set; }
     public DateTime Timestamp { get; set; } = DateTime.UtcNow;
     public DateTime LastEdited { get; set; } = DateTime.UtcNow;
     public PrivateConversationModel Conversation { get; set; }
