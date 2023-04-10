42d5240844da8f1a40cbdc018c557a8bc14547f8
Timothy Tandt <Timothy.tandt@outlook.be>
2023-04-10 16:34:37 +0200

    Added CreateServer.razor and Servers.razor
    Servers.razor displays all the available servers.
    CreateServer allows you to create a server.

---
+++ Chatify/Models/CreateServerModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Chatify.Models;
+
+public class CreateServerModel
+{
+    [Required]
+    [MaxLength(50)]
+    [Display(Name = "Server Name")]
+    public string ServerName { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    [Display(Name = "Server Description")]
+    public string ServerDescription { get; set; }
+
+    [Display(Name = "Picture Name")]
+    public string PictureName { get; set; }
+
+    [Required]
+    [MinLength(1)]
+    [Display(Name = "Category")]
+    public string CategoryId { get; set; }
+}
