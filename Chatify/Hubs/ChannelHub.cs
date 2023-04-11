54ceb2b0f453d231f8428a63b3c32fb93dda6baf
Timothy Tandt <Timothy.tandt@outlook.be>
2023-04-11 20:39:19 +0200

    Added Server.razor
    Added the related ChannelHub for it.
    Added the logic for Server.razor but no HTML have been added.

---
+++ Chatify/Hubs/ChannelHub.cs
@@ -0,0 +1,22 @@
+﻿using ChatifyLibrary.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Chatify.Hubs;
+
+public class ChannelHub : Hub
+{
+    public Task SendMessage(MessageModel message, ChannelModel channel)
+    {
+        return Clients.Group(channel.Id).SendAsync("ReceiveMessage", message);
+    }
+
+    public async Task JoinConversation(ChannelModel channel)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, channel.Id);
+    }
+
+    public async Task LeaveConversation(ChannelModel channel)
+    {
+        await Groups.RemoveFromGroupAsync(Context.ConnectionId, channel.Id);
+    }
+}
