e95d7a2a20fada8fed83cc3571299bba3e7982cf
Timothy Tandt <Timothy.tandt@outlook.be>
2023-04-15 20:29:33 +0200

    Modification for sending messages.
    The message Input is now wrapped in a EditForm, so the message text can't be empty and setting a maximum text.

---
+++ Chatify/Models/CreateMessageModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Chatify.Models;
+
+public class CreateMessageModel
+{
+    [Required]
+    [MaxLength(256)]
+    public string Text { get; set; }
+}
